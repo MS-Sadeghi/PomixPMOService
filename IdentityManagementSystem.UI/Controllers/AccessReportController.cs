2cfa4e2ab5487249eb6042f51814998c40ab692b
Sdi <sdi10fateme@gmail.com>
2026-06-21 09:04:00 +0330

    ساختار اولیه  AccessReport

---
+++ IdentityManagementSystem.UI/Controllers/AccessReportController.cs
@@ -0,0 +1,15 @@
+﻿using IdentityManagementSystem.UI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagementSystem.UI.Controllers
+{
+    public class AccessReportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ IdentityManagementSystem.UI/ViewModels/AccessReportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagementSystem.UI.ViewModels
+{
+    public class AccessReportViewModel
+    {
+        public string ReportDate { get; set; }
+        public string EntranceType { get; set; }
+        public int RecordCount { get; set; }
+    }
+}
